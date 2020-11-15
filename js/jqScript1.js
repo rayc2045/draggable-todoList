501b8a58d01a35c8018456f5bb62421686e2c150
Ray C <rayc2045@gmail.com>
2020-11-15 17:08:15 +0800

    Refactor

---
+++ js/jqScript1.js
@@ -0,0 +1,212 @@
+'use strict';
+
+$(function() {
+	let TodoApp = {
+		init: function() {
+			// this.debug();
+			this.demo_f2eLearning();
+			this.updateTasks();
+			this.addTask();
+			this.toggleComplete();
+			this.editTask();
+			this.deleteTask();
+			// jQueryUISortable();
+			this.sortablejs();
+			this.rearrangeTodolist();
+		},
+		todolist: JSON.parse(localStorage.getItem('todolist')) || [],
+		debug: function() {
+			const self = this;
+
+			$('#todolist').on('click', function() {
+				console.log(self.todolist);
+			});
+
+			$('.newItem input').keydown(function(e) {
+				if (e.which === 13) {
+					console.log('"' + $(this).val() + '" is added.');
+				}
+			});
+
+			$('.items').on('click', '.delete', function() {
+				console.log('"' + $(this).closest('.item').find('.content').text() + '" is deleted.');
+			});
+
+			$('.items').change(function() {
+				console.log('.items changed');
+			});
+		},
+		demo_f2eLearning: function() {
+			localStorage.setItem('todolist', JSON.stringify([
+					{
+						task: 'HTML + CSS 網頁基礎',
+						completed: false
+					},
+					{
+						task: 'Flexbox + CSS Grid 響應式佈局',
+						completed: false
+					},
+					{
+						task: 'JavaScript + DOM 操作',
+						completed: false
+					},
+					{
+						task: 'Git/Github 版本控制',
+						completed: false
+					},
+					{
+						task: '搭建網站/Github page',
+						completed: false
+					},
+					{
+						task: 'Node.js + NPM 套件管理',
+						completed: false
+					},
+					{
+						task: 'React.js/Vue.js 前端框架',
+						completed: false
+					},
+					{
+						task: 'HTTP + AJAX/JSON API 服務整合',
+						completed: false
+					},
+					{
+						task: '作品集 + 一頁式履歷',
+						completed: false
+					},
+					{
+						task: '尋找、取得 offer',
+						completed: false
+					}
+				])
+			);
+		},
+		updateTasks: function() {
+			$('.items').empty();
+
+			for (let i = 0; i < this.todolist.length; i++) {
+				$('.items').append(`
+					<div class="item" id="item_${i}">
+						<div class="handle">: :</div>
+						<input type="checkbox" ${this.todolist[i].completed ? 'checked' : ''}>
+						<div class="check">✓</div>
+						<div class="content">${this.todolist[i].task}</div>
+						<div class="delete">☓</div>
+					</div>`);
+			}
+		},
+		addTask: function() {
+			const self = this;
+
+			$('.newItem input').keydown(function(e) {
+				if (e.which === 13) {
+					// input, select, textarea 用 val；其他使用 text()
+					// val() 會抓取內容，如果輸入值到括號中會變為輸出
+					// trim() 刪除首尾空格
+					const input = $(this).val().trim(); 
+
+					if (!input) return;
+
+					self.todolist.push({task: input, completed: false});
+					localStorage.setItem('todolist', JSON.stringify(self.todolist));
+					self.updateTasks();
+					$(this).val('');
+				}
+			});
+		},
+		toggleComplete: function() {
+			const self = this;
+
+			$('.items').on('click', 'input', function() {
+				const index = $(this).closest('.item').attr('id').replace(/item_/g, '');
+				const accomplishSound = new Audio(
+					// 'http://noproblo.dayjo.org/ZeldaSounds/BOTW/BOTW_Fanfare_SmallItem.wav' // 使用 html 載入 audio 方式能在第一次播放無延遲，但播放中的其他操作無法播放音效
+					'../audio/BOTW_Fanfare_SmallItem.wav' // 使用 js 載入 audio 在第一次播放會有明顯延遲，但播放中的其他操作仍可繼續播放音效
+				);
+
+				self.todolist[index].completed = !self.todolist[index].completed;
+				localStorage.setItem('todolist', JSON.stringify(self.todolist));
+				$('.content').blur();
+
+				if ($(this).is(':checked')) {
+					// $('.accomplishSound')[0].play();
+					accomplishSound.play();
+				}
+			});
+		},
+		editTask: function() {
+			const self = this;
+
+			$('.items').on('mousedown', '.content', function() {
+				$(this).prop('contenteditable', true);
+			});
+
+			$('.items').on('keydown', '.content', function(e) {
+				if (e.which === 13) {
+					$(this).blur();
+				}
+			});
+
+			$('.items').on('blur', '.content', function() {
+				const index = $(this).closest('.item').attr('id').replace(/item_/g, '');
+
+				self.todolist[index].task = $(this).text().trim();
+				localStorage.setItem('todolist', JSON.stringify(self.todolist));
+				$(this).prop('contenteditable', false);
+				self.updateTasks();
+			});
+		},
+		deleteTask: function() {
+			const self = this;
+
+			$('.items').on('click', '.delete', function() {
+				const item = $(this).closest('.item');
+				const index = item.attr('id').replace(/item_/g, '');
+				const deleteSound = new Audio('../audio/BotW_Interact_sound.mp3');
+
+				self.todolist.splice(index, 1);
+				localStorage.setItem('todolist', JSON.stringify(self.todolist));
+				item.remove();
+				self.updateTasks();
+				deleteSound.play();
+			});
+		},
+		jQueryUISortable: function() {
+			$('#todolist .items').sortable({
+				items: '.item',
+				cancel: '.content',
+				placeholder: 'dragging'
+			});
+		},
+		sortablejs: function() {
+			Sortable.create(sortable, {
+				// disabled: false, // 關閉 Sortable
+				animation: 100, // 物件移動時間(單位:毫秒)
+				handle: '.handle', // 可拖曳的區域
+				draggable: '.item', // 可拖曳的物件
+				// filter: ".newItem",  // 過濾器，不能拖曳的物件
+				// preventOnFilter: true, // 當過濾器啟動的時候，觸發 event.preventDefault()
+				ghostClass: 'dragging' // 拖曳時，給予物件的類別
+				// chosenClass: "dragging",  // 選定時，給予物件的類別
+			});
+		},
+		rearrangeTodolist: function() {
+			const self = this;
+
+			$('.items').change(function() {
+				for (let i = 0; i < self.todolist.length; i++) {
+					// 將 TodoApp.todolist 中的資料，複寫為 html 中順序對應的項目值
+					self.todolist[i].task = $('.item').eq(i).find('.content').text();
+					self.todolist[i].completed = $('.item').eq(i).find('input').prop('checked');
+					
+					localStorage.setItem('todolist', JSON.stringify(self.todolist));
+
+					// 重置 html 中項目的 id 值
+					$('.item').eq(i).attr('id', 'item_' + i);
+				}
+			});
+		},
+	};
+
+	TodoApp.init();
+});
---
+++ js/script.js
@@ -0,0 +1,194 @@
+'use strict';
+
+class TodoApp {
+	constructor() {
+		this.todoList = JSON.parse(localStorage.getItem('todoList')) || [
+			{
+				task: '可使用任務左側 ": :" 符號拖曳項目',
+				completed: false
+			},
+			{
+				task: '可使用任務右側 "✕" 符號刪除項目',
+				completed: false
+			},
+			{
+				task: '點擊任務後可自由進行編輯，編輯完成後輸入 "Enter" 將保存紀錄',
+				completed: false
+			},
+			{
+				task: '自動儲存機制將保留任何更動',
+				completed: false
+			},
+			{
+				task: '開始新增您的第一項任務 🙂',
+				completed: false
+			}
+		];
+		this.itemsParentEl = document.querySelector('.items');
+		this.itemEls = this.itemsParentEl.childNodes;
+		this.newItemInputEl = document.querySelector('.new-item-input');
+		this.maxTaskNumber = 10;
+		this.accomplishSound = new Audio(
+			'https://github.com/rayc2045/f2e-portfolio/blob/master/06%20-%20%E5%8F%AF%E6%8B%96%E6%8B%89%E4%BB%BB%E5%8B%99%E7%9A%84%20LocalStorage%20%E5%BE%85%E8%BE%A6%E4%BA%8B%E9%A0%85%E6%B8%85%E5%96%AE/audio/BOTW_Fanfare_SmallItem.wav?raw=true'
+		); // ../audio/BOTW_Fanfare_SmallItem.wav
+		this.deleteSound = new Audio(
+			'https://github.com/rayc2045/f2e-portfolio/blob/master/06%20-%20%E5%8F%AF%E6%8B%96%E6%8B%89%E4%BB%BB%E5%8B%99%E7%9A%84%20LocalStorage%20%E5%BE%85%E8%BE%A6%E4%BA%8B%E9%A0%85%E6%B8%85%E5%96%AE/audio/BotW_Interact_sound.mp3?raw=true'
+		); // ../audio/BotW_Interact_sound.mp3
+		this.dragSound = new Audio(
+			'https://github.com/rayc2045/f2e-portfolio/blob/master/06%20-%20%E5%8F%AF%E6%8B%96%E6%8B%89%E4%BB%BB%E5%8B%99%E7%9A%84%20LocalStorage%20%E5%BE%85%E8%BE%A6%E4%BA%8B%E9%A0%85%E6%B8%85%E5%96%AE/audio/drag.mp3?raw=true'
+		); // ../audio/drag.mp3
+		this.dropSound = new Audio(
+			'https://github.com/rayc2045/f2e-portfolio/blob/master/06%20-%20%E5%8F%AF%E6%8B%96%E6%8B%89%E4%BB%BB%E5%8B%99%E7%9A%84%20LocalStorage%20%E5%BE%85%E8%BE%A6%E4%BA%8B%E9%A0%85%E6%B8%85%E5%96%AE/audio/drop.mp3?raw=true'
+		); // ../audio/drop.mp3
+		this.events();
+	}
+
+	events() {
+    this.updateTasks();
+    
+		// Drag and rearrange
+		this.sortableJS();
+		this.itemsParentEl.onchange = () => this.rearrangeTodoList();
+
+		// Handle sound and content edit
+		this.itemsParentEl.onmousedown = (e) => {
+			if (e.target.classList.contains('handle')) return this.playSound(this.dragSound);
+			if (e.target.classList.contains('content')) this.editEnable(e);
+		};
+
+		// Enter => blur => save content
+		this.itemsParentEl.onkeydown = (e) => {
+			if (e.target.classList.contains('content')) {
+				if (e.which === 13) return e.target.blur();
+				if (e.which === 8 && !e.target.textContent) this.deleteTask(e);
+			}
+    };
+		this.itemEls.forEach((el) => {
+			el.childNodes[7].onblur = (e) => this.saveContent(e);
+		});
+
+		// Complete and delete task
+		this.itemsParentEl.onclick = (e) => {
+			if (e.target.type === 'checkbox') return this.toggleComplete(e);
+			if (e.target.classList.contains('delete')) this.deleteTask(e);
+		};
+
+		// Add task
+		this.newItemInputEl.onkeydown = (e) => {
+			if (this.itemEls.length > this.maxTaskNumber - 1) return this.inputDisable(e);
+			if (e.which === 13) this.addTask();
+		};
+	}
+
+	updateTasks() {
+		this.itemsParentEl.innerHTML = '';
+
+		for (const i in this.todoList) {
+			const item = document.createElement('li');
+			item.id = `item_${i}`;
+			item.classList.add('item');
+			item.innerHTML = `
+        <div class="handle">: :</div>
+        <input type="checkbox"${this.todoList[i].completed ? ' checked' : ''}>
+        <div class="check">✓</div>
+        <div class="content">${this.todoList[i].task}</div>
+        <div class="delete">✕</div>`;
+			this.itemsParentEl.appendChild(item);
+		}
+
+		this.setPlaceholder(this.maxTaskNumber);
+	}
+
+	setPlaceholder(maxNumber) {
+		this.itemEls.length < maxNumber
+			? (this.newItemInputEl.placeholder = 'What needs to be done?')
+			: (this.newItemInputEl.placeholder = 'Try to make tasks less...');
+	}
+
+	addTask() {
+		const input = this.newItemInputEl.value.trim();
+		if (!input) return;
+		this.todoList.push({ task: input, completed: false });
+		this.setLocalStorage('todoList', this.todoList);
+		this.updateTasks();
+		this.newItemInputEl.value = '';
+		this.playSound(this.dropSound);
+	}
+
+	inputDisable(e) {
+		e.preventDefault();
+		this.newItemInputEl.value = '';
+	}
+
+	deleteTask(e) {
+		const item = e.target.closest('.item');
+		const index = item.id.replace(/item_/g, '');
+
+		this.todoList.splice(index, 1);
+		this.todoList.length
+			? this.setLocalStorage('todoList', this.todoList)
+			: this.removeFromLocalStorage('todoList');
+
+		this.updateTasks();
+		this.playSound(this.deleteSound);
+	}
+
+	toggleComplete(e) {
+		e.preventDefault(); // Prevent trigging itemsParentEl onchange event
+		const index = e.target.closest('.item').id.replace(/item_/g, '');
+		this.todoList[index].completed = !this.todoList[index].completed;
+		this.setLocalStorage('todoList', this.todoList);
+		this.updateTasks();
+		if (e.target.checked) this.playSound(this.accomplishSound, 0.35);
+	}
+
+	editEnable(e) {
+		e.target.contentEditable = 'true';
+	}
+
+	saveContent(e) {
+		const index = e.target.closest('.item').id.replace(/item_/g, '');
+		this.todoList[index].task = e.target.textContent.trim();
+		this.setLocalStorage('todoList', this.todoList);
+		this.updateTasks();
+	}
+
+	sortableJS() {
+		Sortable.create(sortable, {
+			// disabled: false, // Turn off Sortable
+			animation: 100, // Animation time: 0.1s
+			handle: '.handle', // Drag area
+			draggable: '.item', // Draggable element
+			// filter: ".new-item",  // Not draggable element
+			// preventOnFilter: true, // If 'filter', e.preventDefault()
+			ghostClass: 'dragging' // Give a class to dragged element
+			// chosenClass: "dragging",  // Give a class to chosen element
+		});
+	}
+
+	rearrangeTodoList() {
+		for (const i in this.todoList) {
+			this.todoList[i].completed = this.itemEls[i].childNodes[3].checked;
+			this.todoList[i].task = this.itemEls[i].childNodes[7].textContent;
+			this.setLocalStorage('todoList', this.todoList);
+			this.itemEls[i].id = `item_${i}`;
+			this.playSound(this.dropSound);
+		}
+	}
+
+	setLocalStorage(title, data) {
+		localStorage.setItem(title, JSON.stringify(data));
+	}
+
+	removeFromLocalStorage(title) {
+		localStorage.removeItem(title); // It's not localStorage.clear()
+	}
+
+	playSound(audio, volume = 1) {
+    audio.currentTime = 0;
+    audio.volume = volume;
+		audio.play();
+	}
+}
+
+new TodoApp();
